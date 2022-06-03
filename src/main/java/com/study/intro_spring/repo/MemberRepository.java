fa006df0e66d08ca9c16c62141cf95f475213658
black9769 <black9769@dnx.kr>
2022-06-03 23:34:57 +0900

    first commit

---
+++ src/main/java/com/study/intro_spring/repo/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.study.intro_spring.repo;
+
+import com.study.intro_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ src/test/java/com/study/intro_spring/IntroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.intro_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
